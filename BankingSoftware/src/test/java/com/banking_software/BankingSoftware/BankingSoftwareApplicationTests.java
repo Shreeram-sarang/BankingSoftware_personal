94350c850d96b45d428c7d42725579054557f184
Shreeram-sarang <118298146+Shreeram-sarang@users.noreply.github.com>
2024-10-23 23:00:26 +0530

    Add files via upload

---
+++ BankingSoftware/src/test/java/com/banking_software/BankingSoftware/BankingSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking_software.BankingSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
